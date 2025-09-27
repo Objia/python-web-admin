2396ec87c4086cd97e93f3cffd2b987f32e5f151
Bob-home <bobjamth@163.com>
2025-09-28 00:21:59 +0800

    mysql数据库配置

--- python_web_admin/settings.py
+++ python_web_admin/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_web_admin',
+        'NAME': 'python_web_admin', #数据库名称
         'USER': 'root',
         'PASSWORD': 'WBl2t$H=',
         'HOST': 'localhost',
